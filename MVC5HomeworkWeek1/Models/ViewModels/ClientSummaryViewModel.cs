6f9085411d5c508099f0e19fdc4f500246224f60
TONY <TONY@15000651TONY>
2017-05-10 21:46:42 +0800

    Add ViewModel & View for Client Summary

---
+++ MVC5HomeworkWeek1/Models/ViewModels/ClientSummaryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5HomeworkWeek1.Models.ViewModels
+{
+    public class ClientSummaryViewModel
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 客戶名稱
+        /// </summary>
+        public int Name { get; set; }
+
+        /// <summary>
+        /// 聯絡人數量
+        /// </summary>
+        public int ContactTotal { get; set; }
+
+        /// <summary>
+        /// 銀行帳戶數量
+        /// </summary>
+        public int AccountTotal { get; set; }
+    }
+}
